9e052ae70d8373d39cdd33970c7fa2e2f61ff69f
Szczepan <szczep.kowalik@gmail.com>
2022-12-11 15:51:41 +0100

    Added authentication Register and Login methods

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,25 +1,90 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Drinks_app.Models.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using ContactsAppAPI.Authentication.Models;
+using ContactsAppAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Drinks_app.Models.Authentication;
+using Drinks_app.Models;
 
-namespace Drinks_app.Controllers
+namespace ContactsAppAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
-        [HttpPost("register")]
-        public async Task<IActionResult> Register()
-        {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
 
-            return Ok();
+        public AuthenticationController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var reguest = Request;
+            var user = await _userManager.FindByNameAsync(model.Username);
+            var checkPswrd = await _userManager.CheckPasswordAsync(user, model.Password);
+            if (user != null && checkPswrd)
+            {
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["JWT:ValidIssuer"],
+                    audience: _configuration["JWT:ValidAudience"],
+                    expires: DateTime.Now.AddHours(3),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+                return Ok(new
+                {
+                    username = user.UserName,
+                    email = user.Email,
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized("Invalid credentials");
         }
 
-        [HttpPost("login")]
-        public async Task<IActionResult> Login()
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
         {
-
-            return Ok();
+            var userExists = await _userManager.FindByEmailAsync(model.Email);
+            if (userExists != null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
+            }
+            ApplicationUser user = new ApplicationUser
+            {
+                Email = model.Email,
+                UserName = model.Username,
+                SecurityStamp = Guid.NewGuid().ToString(),
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+            }
+            return Ok(new Response { Status = "Success", Message = "User created successfully!" });
         }
     }
 }
