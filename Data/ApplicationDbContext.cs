cd70d6ce2834e96dd9a5fed1e4d874c831cdbe7a
Szczepan <szczep.kowalik@gmail.com>
2022-12-11 16:19:38 +0100

    Added ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Drinks_app.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace Drinks_app.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<CocktailRecipe> CocktailRecipes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+    }
+}
