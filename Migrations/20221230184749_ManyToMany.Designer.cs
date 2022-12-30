d00722a13fd88b5336f54871fa29dbbbadc34fbc
Mikolaj Tanski <mikolajtanski1@gmail.com>
2022-12-30 19:48:13 +0100

    many to many migracjon

--- Migrations/20221230184749_ManyToMany.Designer.cs
+++ Migrations/20221230184749_ManyToMany.Designer.cs
@@ -4,14 +4,16 @@ using Drinks_app.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Drinks_app.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221230184749_ManyToMany")]
+    partial class ManyToMany
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
