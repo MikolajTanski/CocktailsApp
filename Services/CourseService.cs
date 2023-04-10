14174e0bd8a3ee1c4b353de5498b4ff6970467fe
Szczepan <szczep.kowalik@gmail.com>
2023-04-10 18:01:27 +0200

    Changed CourseService to work with both Header and Content

--- Services/CourseService.cs
+++ Services/CourseService.cs
@@ -1,33 +1,45 @@
 ﻿using Drinks_app.Models;
 using Drinks_app.Models.DTO;
 using Drinks_app.Services.IServices;
+using Drinks_app.Repositories.IRepositories;
 using System.Collections.Generic;
 
 namespace Drinks_app.Services
 {
     public class CourseService : ICourseService
     {
-        public void CreateCourseHeader(CourseHeader courseHeader)
+        private readonly ICourseHeaderRepository _courseHeadRepo;
+        private readonly ICourseContentRepository _courseContentRepo;
+
+        public CourseService(
+            ICourseHeaderRepository courseHeadRepo,
+            ICourseContentRepository courseContentRepo)
+        {
+            _courseHeadRepo = courseHeadRepo;
+            _courseContentRepo = courseContentRepo;
+        }
+
+        public void CreateCourse(CourseDto courseDto)
         {
             throw new System.NotImplementedException();
         }
 
-        public void DeleteCourseHeader(long id)
+        public void DeleteCourse(long id)
         {
             throw new System.NotImplementedException();
         }
 
-        public CourseHeader GetCourseHeaderById(long id)
+        public CourseDto GetCourseById(long id)
         {
             throw new System.NotImplementedException();
         }
 
-        public IEnumerable<CourseHeader> GetCourseHeaders()
+        public IEnumerable<CourseDto> GetCourses()
         {
             throw new System.NotImplementedException();
         }
 
-        public void UpdateCourseHeader(CourseHeader courseHeader)
+        public void UpdateCourse(CourseDto courseDto)
         {
             throw new System.NotImplementedException();
         }
