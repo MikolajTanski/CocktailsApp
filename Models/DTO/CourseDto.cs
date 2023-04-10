7f7de7f2ce8dcbda3c358346477d6220369eaa34
Szczepan <szczep.kowalik@gmail.com>
2023-04-10 17:36:14 +0200

    Created CourseService with DTO

---
+++ Models/DTO/CourseDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Drinks_app.Models.DTO
+{
+    public class CourseDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<CourseContent> CourseContent { get; set; }
+        public Category Category { get; set; }
+        public ApplicationUser Author { get; set; }
+    }
+}
---
+++ Services/CourseService.cs
@@ -0,0 +1,35 @@
+﻿using Drinks_app.Models;
+using Drinks_app.Models.DTO;
+using Drinks_app.Services.IServices;
+using System.Collections.Generic;
+
+namespace Drinks_app.Services
+{
+    public class CourseService : ICourseService
+    {
+        public void CreateCourseHeader(CourseHeader courseHeader)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCourseHeader(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public CourseHeader GetCourseHeaderById(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<CourseHeader> GetCourseHeaders()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCourseHeader(CourseHeader courseHeader)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
